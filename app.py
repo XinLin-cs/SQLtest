9e62697350a17fb857db9a94d9875063fb029376
ArComet <44643165+ArComet@users.noreply.github.com>
2021-06-16 21:49:08 +0800

    1.1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hello-world-jcfuns'  # CSRF保护密钥
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://testServer:123456@sqltest'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost:3306/sqltest'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 db = SQLAlchemy(app)
 
